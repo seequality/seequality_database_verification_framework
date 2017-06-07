1220317f429ecae87546fe0eaec62dc5ac880293
Slawomir Drzymala <slawomir.drzymala@betssongroup.com>
2017-06-07 19:47:59 +0200

    Adding the files wiwth the verification and data verification queries. Basic templates and checking data

---
+++ ExecutionVerificationScripts.sql
@@ -0,0 +1,25 @@
+﻿USE SSIS_DataVerificationFreamwork;
+
+DECLARE @LastExecution UNIQUEIDENTIFIER
+DECLARE @PreviousExecution UNIQUEIDENTIFIER
+
+;WITH cte AS 
+(
+	SELECT 
+		ExecutionID, 
+		ROW_NUMBER() OVER	(ORDER BY ExecutionTime desc) AS RN
+	FROM Internal.ExecutionLog
+)
+SELECT 
+	@LastExecution = (SELECT ExecutionID FROM cte WHERE RN = 1),
+	@PreviousExecution = (SELECT ExecutionID FROM cte WHERE RN = 2)
+
+
+SELECT * 
+FROM internal.ColumnInput a
+FULL OUTER JOIN internal.ColumnInput b 
+	ON a.ID = b.ID
+WHERE a.ExecutionID = @LastExecution
+AND b.ExecutionID = @PreviousExecution
+--AND a.ID <> b.ID
+
